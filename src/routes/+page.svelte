749d02ff58044e310591c790353fe9b3dc3a00bf
yang siyu <164805339+yangsiyu616@users.noreply.github.com>
2026-03-25 17:28:53 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
     console.log("hello world")
+    let count = $state(2)
+    let doubled = $derived (count * 2)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<button onclick={()=>count++}>
+    clicks:{count}
+</button>
+
+<span>doubled is {doubled}</span>
+
 <style>
     h1 {
         color: deepskyblue;
     }
-
 </style>
