475ca6a79d922f507697f403079714db751634cf
yang siyu <164805339+yangsiyu616@users.noreply.github.com>
2026-03-25 17:40:29 +0100

    clicks counter

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,12 @@
+<script>
+    let count = $state(2)
+    let doubled = $derived (count * 2)
+</script>
+
+<h3>Clicks Counter</h3>
+
+<button onclick={()=>count++}>
+    clicks:{count}
+</button>
+
+<span>doubled is {doubled}</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,14 @@
 <script>
-    console.log("hello world")
     let count = $state(2)
     let doubled = $derived (count * 2)
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte"
+    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte"
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<button onclick={()=>count++}>
-    clicks:{count}
-</button>
-
-<span>doubled is {doubled}</span>
+<ClicksCounter />
 
 <style>
     h1 {
